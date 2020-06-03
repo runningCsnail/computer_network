e7c25fef1c80729c485f93ba7169e973cb5ac285
Clifford Chen <39901127+runningCsnail@users.noreply.github.com>
2020-06-04 03:51:16 +0800

    Update UDPClient.py

--- UDPClient.py
+++ UDPClient.py
@@ -8,6 +8,3 @@ clientSocket.sendto(message.encode(), (serverName, serverPort))
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
 print(modifiedMessage.decode())
 clientSocket.close()
-
-#https://stackoverflow.com/questions/2582036/an-existing-connection-was-forcibly-closed-by-the-remote-host?rq=1
-#https://stackoverflow.com/questions/46223078/c-sharp-httpclient-an-existing-connection-was-forcibly-closed-by-the-remote-host?noredirect=1&lq=1
